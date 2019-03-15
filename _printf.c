db34381c6f8383cb1a9f25eb3b3b78127c38509c
Jason Cortella <419@holbertonschool.com>
2019-03-15 23:20:32 +0000

    initial setup commit

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include holberton.h
+
+/**
+ *_printf - prints input as printf would do
+ * @format: a character string containing directives
+ * Return: The number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
