e6fca683e2598d645a4a64be1559150f26be4890
Jason Cortella <419@holbertonschool.com>
2019-03-15 23:42:39 +0000

    added struct declaration

--- holberton.h
+++ holberton.h
@@ -1,6 +1,13 @@
 #idndef HOLBERTON_H
 #define HOLBERTON_H
 
+typedef struct print
+{
+	char *type;
+	void (*func)(va_list);
+} print_table;
+
+int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
