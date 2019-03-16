f3707cee770df806c542a69352144954dba99332
ethanpasta <ethan.mayer9@gmail.com>
2019-03-16 23:24:38 +0000

    Add return statement of number of characters printed

--- _printf.c
+++ _printf.c
@@ -1,4 +1,4 @@
-#include holberton.h
+#include "holberton.h"
 
 /**
  *_printf - prints input as printf would do
@@ -8,19 +8,46 @@
 
 int _printf(const char *format, ...)
 {
-	int i, count;
-	va_list valist;
-	print_table p[] = {
-		{"c", print_char},
-		{"s", print_str}
-		{NULL, NULL}
+	int i, j, count = 0;
+	char ptype;
+	va_list args;
+	print_table ptbl[] = {
+		{'c', print_c},
+		{'s', print_s},
+		{'%', print_perc},
+		{0, NULL}
 	};
 
-	va_start(valist, format);
-
-	/*
-	 * INSERT PRINTF FUNCTIONALITY HERE
-	 */
-
-	va_end(valist);
+	va_start(args, format);
+	while (format && format[i])
+	{
+		if (!format[i])
+			break;
+		while (format[i] != '%')
+		{
+			_putchar(format[i]);
+			count++;
+			i++;
+		}
+		if (!format[i])
+			break;
+		i++;
+		ptype = format[i];
+		for (j = 0; ptbl[j].type; j++)
+		{
+			if (ptype == ptbl[j].type)
+			{
+				count += ptbl[j].func(args);
+				break;
+			}
+		}
+		if (ptbl[j].type == 0)
+		{
+			_putchar('%');
+			_putchar(format[i]);
+			count += 2;
+		}
+	}
+	va_end(args);
+	return (count);
 }
