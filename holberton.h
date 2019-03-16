e5c4ba1c47143762b91a8726c61217c4514e8f23
ethanpasta <ethan.mayer9@gmail.com>
2019-03-16 22:42:06 +0000

    Add function prototypes

--- holberton.h
+++ holberton.h
@@ -1,5 +1,6 @@
 #idndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdarg.h>
 
 typedef struct print
 {
@@ -9,5 +10,8 @@ typedef struct print
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+void print_c(va_list args);
+void print_s(va_list args);
+void print_perc(va_list args);
 
 #endif
