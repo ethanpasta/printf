69e8493670ae84dc92301edb36b57aa8a4acfa81
ethanpasta <ethan.mayer9@gmail.com>
2019-03-16 23:23:44 +0000

    Change function prototypes

--- holberton.h
+++ holberton.h
@@ -1,17 +1,18 @@
-#idndef HOLBERTON_H
+#ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <stdarg.h>
+#include <stdlib.h>
 
 typedef struct print
 {
-	char *type;
-	void (*func)(va_list);
+	char type;
+	int (*func)(va_list);
 } print_table;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-void print_c(va_list args);
-void print_s(va_list args);
-void print_perc(va_list args);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_perc(va_list args);
 
 #endif
