f6427f66f2b2de1e416fb13b31e2aafbb81b3b88
ethanpasta <ethan.mayer9@gmail.com>
2019-03-16 23:30:09 +0000

    Main test file

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int len;
+	int len2;
+	unsigned int ui;
+	void *addr;
+
+	len = _printf("US:    Let's try to printf a simple sentence.\n");
+	len2 = printf("THEM:  Let's try to printf a simple sentence.\n");
+	/*ui = (unsigned int)INT_MAX + 1024;
+	addr = (void *)0x7ffe637541f0;
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("Unsigned:[%u]\n", ui);
+	printf("Unsigned:[%u]\n", ui);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
+	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);*/
+	_printf("US:   Character:[%c]\n", 'H');
+	printf("THEM:   Character:[%c]\n", 'H');
+	_printf("US:  String:[%s]\n", "I am a string !");
+	printf("THEM:  String:[%s]\n", "I am a string !");
+	//_printf("Address:[%p]\n", addr);
+	//printf("Address:[%p]\n", addr);
+	len = _printf("US:  Percent:[%%]\n");
+	len2 = printf("THEM:  Percent:[%%]\n");
+	//_printf("Len:[%d]\n", len);
+	//printf("Len:[%d]\n", len2);
+	_printf("US:   Unknown:[%r]\n");
+	printf("THEM:   Unknown:[%r]\n");
+	return (0);
+}
