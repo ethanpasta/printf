25b7b53e9b1d42efa5f75c9b7532b172d6b39fb4
Jason Cortella <419@holbertonschool.com>
2019-03-19 21:31:40 +0000

    Update readme

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+#include <stdio.h>
+
+int main(void)
+{
+	_printf("%R\n", "Hello, world");
+	printf("%X\n", 15*15);
+//	_printf("String backwards is %r\n", "String");
+	return (0);
+}
